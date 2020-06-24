b1ece51cac62c7388c91d5c4b8d177a858ac42b0
kejgon <kejgon.gmail.com>
2020-06-24 10:42:49 +0300

    submit

--- app.js
+++ app.js
@@ -45,7 +45,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 
@@ -148,11 +149,42 @@ app.get("/register", function (req, res) {
 
 app.get("/secrets", function (req, res) {
 
+    User.find({ "secret": { $ne: null } }, function (err, foundusers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundusers) {
+                res.render("secrets", { usersWithSecret: foundusers });
+            }
+        }
+
+    });
+});
+
+////! SUBMIT
+app.get("/submit", function (req, res) {
+
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+
 });
 
 app.get("/logout", function (req, res) {
